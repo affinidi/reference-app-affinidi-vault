70c1a134ccac777ec4a70c3ade5fb414905fe88e
Sanjay Kumar <sanjaysshekhar@live.com>
2024-01-08 16:04:42 +0530

    Generated dotnet sample app

---
+++ samples/affinidi-dotnet-openidconnect/Program.cs
@@ -0,0 +1,16 @@
+  namespace Affinidi_Login_Demo_App;
+  public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(config =>
+                {
+                    config.UseStartup<Startup>();
+                });
+
+    }

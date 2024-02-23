a62404450e24d825c1bfdb5ac59df168fa599044
Sanjay Kumar <sanjaysshekhar@live.com>
2024-02-23 13:02:59 +0530

    dotnet parsing issue workaround for type data mapping in default config

--- samples/affinidi-dotnet-openidconnect/Pages/UserDetail.cshtml.cs
+++ samples/affinidi-dotnet-openidconnect/Pages/UserDetail.cshtml.cs
@@ -16,9 +16,9 @@ namespace Affinidi_Login_Demo_App
         public string? Username { get; set; }
         public string? AccessToken { get; set; }
         public string? RefreshToken { get; set; }
-        public string? DID {get;set;}
-        public string? Email {get;set;}
-        UserData userData=new UserData(default,default,default,default);
+        public string? DID { get; set; }
+        public string? Email { get; set; }
+        UserData userData = new UserData(default, default, default, default);
 
 
         private readonly TokenClient tokenClient = tokenClient;
@@ -26,24 +26,35 @@ namespace Affinidi_Login_Demo_App
         public async Task OnGet()
         {
             ClaimsPrincipal user = this.User;
-            List<string> claims= user.FindAll("Custom").Select(x=>x.Value).ToList();
-            foreach(var claim in claims)
+            List<string> claims = user.FindAll("Custom").Select(x => x.Value).ToList();
+            Console.WriteLine(String.Join("\n", claims));
+            foreach (var claim in claims)
             {
-                 var dataDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(claim);
-                 userData= userData with
-                 {
-                    given_name=dataDict?.GetValueOrDefault("givenName",userData.given_name),
-                    family_name=dataDict?.GetValueOrDefault("familyName",userData.family_name),
-                    email=dataDict?.GetValueOrDefault("email",userData.email),
-                    did= dataDict?.GetValueOrDefault("did",userData.did)
-                 };
+                try
+                {
+                    var dataDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(claim);
+                userData = userData with
+                {
+                    given_name = dataDict?.GetValueOrDefault("givenName", userData.given_name),
+                    family_name = dataDict?.GetValueOrDefault("familyName", userData.family_name),
+                    email = dataDict?.GetValueOrDefault("email", userData.email),
+                    did = dataDict?.GetValueOrDefault("did", userData.did)
+                };
+                }
+                catch (Exception e)
+                {
+                    //continue to get next claims in case of parsing error due to data in other format 
+                    //than simple key value pair. This is just for this demo app, actual data need to be taken care in real app 
+                    Console.WriteLine(e);
+                }
+
+                
             }
-            
-            // var givenName = user.FindFirstValue("given_name");
-            // var familyName = user.FindFirstValue("family_name");
-            this.Username = userData.given_name + " " + userData.family_name;
-            this.DID=userData.did;
-            this.Email=userData.email;
+
+           
+            this.Username = !String.IsNullOrEmpty(userData.given_name)? userData.given_name + " " + userData.family_name:userData.email.Split('@').ElementAtOrDefault(0);
+            this.DID = userData.did;
+            this.Email = userData.email;
             this.AccessToken = await this.tokenClient.GetAccessToken(this.HttpContext);
             this.RefreshToken = await this.tokenClient.GetRefreshToken(this.HttpContext);
         }

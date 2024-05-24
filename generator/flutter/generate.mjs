3fcd0e892601da8b378bc5342172a5028d46e114
Chaitanya Samprajan <csamprajan@Mac-C02G90SCMD6R.local>
2024-05-24 12:04:39 +0530

    Updated the exclude files

--- generator/flutter/generate.mjs
+++ generator/flutter/generate.mjs
@@ -5,7 +5,7 @@ import mkdirp from 'mkdirp'
 import rimraf from 'rimraf'
 import { generateAppInformation } from '../generateAppInformation.mjs'
 
-const filesToIgnore = ['.env', 'generator-config.json', '.gitkeep', 'appInformation.json']
+const filesToIgnore = [ '.env', 'generator-config.json', '.gitkeep', 'keys', 'appInformation.json']
 const pathsToOverwrite = []
 
 const __dirname = dirname(url.fileURLToPath(import.meta.url))

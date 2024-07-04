e821b0e808d88b0e8fc8b540fb0fdaf35975819d
Anton Bergman <anton.b@affinidi.com>
2024-07-04 15:37:44 +0800

    feat: simpliy (?) issuance page

--- generator/nextjs/template/src/pages/_app.tsx
+++ generator/nextjs/template/src/pages/_app.tsx
@@ -3,17 +3,22 @@ import type { AppProps } from "next/app";
 import NavBar from "src/components/NavBar";
 
 import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <SessionProvider>
-      <NavBar />
-      <div className="mx-auto container py-8 px-4">
-        <Component {...pageProps} />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <NavBar />
+        <div className="mx-auto container py-8 px-4">
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
     </SessionProvider>
   );
 }

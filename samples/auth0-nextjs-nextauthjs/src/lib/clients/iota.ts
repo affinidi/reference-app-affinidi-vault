5e43139432a7ec5ca6b3cb99fffd44ecf4576150
Roman Brazhnyk <roman.b@affinidi.com>
2024-10-07 17:12:56 +0800

    feat: FTL-16871 integrate with redirect flow

--- samples/auth0-nextjs-nextauthjs/src/lib/clients/iota.ts
+++ samples/auth0-nextjs-nextauthjs/src/lib/clients/iota.ts
@@ -2,7 +2,14 @@ import {
   Configuration,
   ConfigurationsApi,
   PexQueryApi,
+  IotaApi,
+  CallbackApi,
+  IotaConfigurationDtoModeEnum,
+  InitiateDataSharingRequestOKData,
+  FetchIOTAVPResponseOK,
 } from "@affinidi-tdk/iota-client";
+import { v4 as uuidv4 } from "uuid";
+import * as jose from "jose";
 import { getAuthProvider } from "./auth-provider";
 
 export async function listIotaConfigurations() {
@@ -10,10 +17,22 @@ export async function listIotaConfigurations() {
   const api = new ConfigurationsApi(
     new Configuration({
       apiKey: authProvider.fetchProjectScopedToken.bind(authProvider),
-    }),
+    })
   );
   const { data } = await api.listIotaConfigurations();
-  return data.configurations;
+  console.log(data)
+  return data.configurations.filter((config) => config.mode === IotaConfigurationDtoModeEnum.Websocket);
+}
+
+export async function listIotaRedirectConfigurations() {
+  const authProvider = getAuthProvider();
+  const api = new ConfigurationsApi(
+    new Configuration({
+      apiKey: authProvider.fetchProjectScopedToken.bind(authProvider),
+    })
+  );
+  const { data } = await api.listIotaConfigurations();
+  return data.configurations.filter((config) => config.mode === IotaConfigurationDtoModeEnum.Redirect);
 }
 
 export async function listPexQueriesByConfigurationId(configurationId: string) {
@@ -21,8 +40,60 @@ export async function listPexQueriesByConfigurationId(configurationId: string) {
   const api = new PexQueryApi(
     new Configuration({
       apiKey: authProvider.fetchProjectScopedToken.bind(authProvider),
-    }),
+    })
   );
   const { data } = await api.listPexQueries(configurationId);
   return data.pexQueries;
+}
+
+export async function initiateDataSharingRequest(
+  configurationId: string,
+  queryId: string,
+  redirectUri: string,
+  nonce: string
+) {
+  const authProvider = getAuthProvider();
+  const api = new IotaApi(
+    new Configuration({
+      apiKey: authProvider.fetchProjectScopedToken.bind(authProvider),
+    })
+  );
+
+  const { data: dataSharingRequestResponse } =
+    await api.initiateDataSharingRequest({
+      configurationId,
+      mode: IotaConfigurationDtoModeEnum.Redirect,
+      queryId,
+      correlationId: uuidv4(),
+      nonce,
+      redirectUri,
+  });
+
+  const { correlationId, transactionId, jwt } =
+    dataSharingRequestResponse.data as InitiateDataSharingRequestOKData;
+  return { correlationId, transactionId, jwt };
+}
+
+export async function fetchIotaVpResponse(
+  configurationId: string,
+  correlationId: string,
+  transactionId: string,
+  responseCode: string
+) {
+  const authProvider = getAuthProvider();
+  const api = new IotaApi(
+    new Configuration({
+      apiKey: authProvider.fetchProjectScopedToken.bind(authProvider),
+    })
+  );
+
+  const iotaVpResponse: FetchIOTAVPResponseOK = await api.fetchIotaVpResponse({
+    configurationId,
+    correlationId,
+    transactionId,
+    responseCode,
+  });
+
+  const vp = JSON.parse((iotaVpResponse.data as any).vpToken);
+  return { vp: vp, nonce: iotaVpResponse.data.nonce };
 }
